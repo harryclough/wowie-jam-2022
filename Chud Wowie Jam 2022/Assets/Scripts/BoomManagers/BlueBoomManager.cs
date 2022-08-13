119f818ff81654245eb0517f172e65a333863631
harryclough <harry.clough@gmail.com>
2022-08-13 10:54:00 +0100

    Refactor sorting and chaining to be faster

--- Chud Wowie Jam 2022/Assets/Scripts/BoomManagers/BlueBoomManager.cs
+++ Chud Wowie Jam 2022/Assets/Scripts/BoomManagers/BlueBoomManager.cs
@@ -8,7 +8,9 @@ public class BlueBoomManager : BoomManager
 {
     [SerializeField] float chainRadius = 2.5f;
     [SerializeField] float speed = 10f;
-    private List<Collider2D> chainedEnemies = new List<Collider2D>();
+    [SerializeField] float hitRadius = 0.1f;
+    // private List<Collider2D> chainedEnemies = new List<Collider2D>();
+    private HashSet<Collider2D> chainedEnemiesSet = new HashSet<Collider2D>();
     private GameObject obj;
     private bool chasing;
     private Homing homing;
@@ -23,11 +25,26 @@ public class BlueBoomManager : BoomManager
         Debug.Log("Blue sheep is boomed");
     }
 
-    //A recursive function to find the closest enemy to the sheep. If there is an enemy, move the sheep to that enemy and then invoke hitEnemiesInRadius, then repeating on the next closest enemy if it is within chain radius
+    // A recursive function to find the closest enemy to the sheep. If there is an enemy, move the sheep to that enemy and then invoke hitEnemiesInRadius, then repeating on the next closest enemy if it is within chain radius
     void Chain(GameObject gameObject)
     {
         Collider2D[] hitColliders = Physics2D.OverlapCircleAll(gameObject.transform.position, chainRadius);
         hitColliders = SortByDistance(hitColliders);
+
+        // Feel free to use your code but this might be a little cleaner.
+        // I would recommended using a HashSet or HashMap instead of a list of chained enemies,
+        // since a list.Contains is O(n) whereas map/set.Contains is O(1)
+        // (Gotta make my data structures lecturer happy lol)
+        foreach (Collider2D collider in hitColliders)
+        {
+            if (collider.tag == "Enemy" && !chainedEnemiesSet.Contains(collider))
+            {
+                chainedEnemiesSet.Add(collider);
+                homing.target = collider.gameObject;
+                return;
+            }
+        }
+        /*
         int i = 0;
         Debug.Log("HitColliders length: " + hitColliders.Length);
         while (i < hitColliders.Length)
@@ -40,14 +57,25 @@ public class BlueBoomManager : BoomManager
             }
             i++;
         }
+         chainedEnemies.Clear();
+        */
+        chainedEnemiesSet.Clear();
         chasing = false;
         homing.enabled = false;
-        chainedEnemies.Clear();
     }
 
-    //A function to sort a array of enemies by distance from the sheep and return the sorted array using bubble sort
+    // A function to sort a array of enemies by distance from the sheep and return the sorted array
     Collider2D[] SortByDistance(Collider2D[] hitColliders)
     {
+        // We don't need to manually implement a sorting algorithm if we just List.Sort()
+        List<Collider2D> hits = new(hitColliders);
+        hits.Sort(delegate(Collider2D lhs, Collider2D rhs) {
+            float lhsSqrDist = (transform.position - lhs.transform.position).sqrMagnitude;
+            float rhsSqrDist = (transform.position - rhs.transform.position).sqrMagnitude;
+            return lhsSqrDist.CompareTo(rhsSqrDist);
+        });
+        return hits.ToArray();
+        /*
         for (int i = 0; i < hitColliders.Length; i++)
         {
             for (int j = 0; j < hitColliders.Length - 1; j++)
@@ -61,29 +89,30 @@ public class BlueBoomManager : BoomManager
             }
         }
         return hitColliders;
+        */
     }
 
 
-    //A function to move forward until it hits the target enemy and then invoke chain
+    // A function to move forward until it hits the target enemy and then invoke chain
     public void Chase(GameObject gameObject)
     {
         MoveForward(gameObject);
-        //check if the sheep has reached the target enemy
-        if (chasing && Vector2.Distance(gameObject.transform.position, homing.target.transform.position) < 0.1f)
+        // check if the sheep has reached the target enemy
+        if (chasing && Vector2.Distance(gameObject.transform.position, homing.target.transform.position) < hitRadius)
         {
             HitEnemiesInRadius(gameObject);
             Chain(gameObject);
         }
     }
 
-    //A function to make the object move forward at a fixed speed in 2D space
+    // A function to make the object move forward at a fixed speed in 2D space
     public void MoveForward(GameObject gameObject)
     {
         gameObject.transform.position += gameObject.transform.right * Time.deltaTime * speed;
     }
 
     private void Start(){
-        //create a component Homing to this game object
+        // create a component Homing to this game object
         homing = gameObject.AddComponent<Homing>();
         homing.enabled = false;
     }
